4c3b55b575c167695dbe60b89498d44e2d63f521
Catherine Brett <catherinejanebrett@icloud.com>
2024-03-06 15:39:26 +0000

    more edits

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import React from "react";
 function App() {
   return (
     <>
-      <h1>Hello world</h1>
+      <h1>Hello everybody</h1>
       <p>Making a change</p>
     </>
   );
 function App(){
   
 
+  return <h1>Hello world</h1>
+  <p>Hi Liv, don't mind me, I'm just making some changes.</p>
 }
 
 export default App;
