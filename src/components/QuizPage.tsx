591d59edb8182e620acead27cdc7072dce741ebf
Olivia Darby <oliviadarby@live.co.uk>
2024-03-07 20:12:23 +0000

    quiz is working

--- src/components/QuizPage.tsx
+++ src/components/QuizPage.tsx
@@ -7,12 +7,39 @@ function QuizPage() {
   const [questions, setQuestions] = React.useState(null);
   console.log(questions);
 
+  const [playerScore, setPlayerScore] = React.useState(0);
+
+  const [revealScore, setRevealScore] = React.useState(false);
+
+  const [numberOfAnswers, setNumberOfAnswers] = React.useState(0);
+
+  function handlePlayerScoreIncrease() {
+    setPlayerScore(playerScore + 1);
+  }
+
+  function revealScoreClick() {
+    setRevealScore(true);
+  }
+
+  function increaseAnswers() {
+    setNumberOfAnswers(numberOfAnswers + 1);
+  }
+
+  // 1) piece of state that we can update for the players selected answer
+  // 2) this starts as an empty array
+  // 3) object shape with id property, isCorrect
+
   function handleGenerateQuizClick() {
     fetchQuiz();
+    setRevealScore(false);
+    setPlayerScore(0);
+    setNumberOfAnswers(0);
   }
 
   async function fetchQuiz() {
-    const resp = await fetch("https://opentdb.com/api.php?amount=10&type=multiple");
+    const resp = await fetch(
+      "https://opentdb.com/api.php?amount=10&type=multiple"
+    );
     const data = await resp.json();
     setQuestions(data.results);
   }
@@ -41,19 +68,32 @@ function QuizPage() {
       </section>
       <section className="section">
         <div className="container">
-          {!questions ? "Generating questions..." :
-          questions.map((element, i) => {
-            return (
-              <ShowQuestions
-                key={i}
-                question={element.question}
-                correct={element.correct_answer}
-                incorrect={element.incorrect_answers}
-              />
-            );
-          })}
+          {!questions
+            ? "Generating questions..."
+            : questions.map((element, i) => {
+                return (
+                  <ShowQuestions
+                    increaseFunction={handlePlayerScoreIncrease}
+                    increaseNoOfAnswers={increaseAnswers}
+                    reveal={revealScore}
+                    key={element.question}
+                    id={i}
+                    question={element.question}
+                    correct={element.correct_answer}
+                    incorrect={element.incorrect_answers}
+                  />
+                );
+              })}
         </div>
       </section>
+      <button
+        className="button"
+        disabled={numberOfAnswers !== 10}
+        onClick={revealScoreClick}
+      >
+        Reveal score
+      </button>
+      {revealScore && <p>Score is: {playerScore}</p>}
     </>
   );
 }
--- src/components/ShowQuestions.tsx
+++ src/components/ShowQuestions.tsx
@@ -1,21 +1,30 @@
-import React from "react"
+import React from "react";
 
-function ShowQuestions({ question, correct, incorrect }) {
+function ShowQuestions({
+  reveal,
+  question,
+  correct,
+  incorrect,
+  id,
+  increaseFunction,
+  increaseNoOfAnswers,
+}) {
   const allAnswers = [...incorrect, correct];
   console.log("All answers are: ", allAnswers);
-  const shuffledAnswers = [...allAnswers.sort()];
+  const shuffledAnswers = [...allAnswers].sort();
   console.log("Shuffled answers are: ", shuffledAnswers);
 
-  const [selectedAnswer, setSelectedAnswer] = React.useState(null)
-  console.log(selectedAnswer)
-
-  let playerScore = 0
+  const [selectedAnswer, setSelectedAnswer] = React.useState(null);
+  console.log(selectedAnswer);
 
   function handleAnswerSelection(e: any) {
-    if (e.currentTarget.innerText === correct) {
-      playerScore += 1 
+    setSelectedAnswer(e.currentTarget.value);
+    increaseNoOfAnswers();
+    console.log(e.currentTarget.value);
+    console.log(correct);
+    if (e.currentTarget.value === correct) {
+      increaseFunction();
     }
-    console.log(playerScore)
   }
 
   return (
@@ -23,24 +32,52 @@ function ShowQuestions({ question, correct, incorrect }) {
       <div className="container">
         <h6>{question}</h6>
         <div className="control">
-          <label className="radio" onChange={handleAnswerSelection}>
-            <input type="radio" name="answer"/>
+          <label className="radio">
+            <input
+              disabled={!!selectedAnswer}
+              onChange={handleAnswerSelection}
+              type="radio"
+              name={`answer-${id}`}
+              value={shuffledAnswers[0]}
+            />
             {shuffledAnswers[0]}
           </label>
-          <label className="radio" onChange={handleAnswerSelection}>
-            <input type="radio" name="answer"/>
+          <label className="radio">
+            <input
+              disabled={!!selectedAnswer}
+              onChange={handleAnswerSelection}
+              type="radio"
+              name={`answer-${id}`}
+              value={shuffledAnswers[1]}
+            />
             {shuffledAnswers[1]}
           </label>
-          <label className="radio" onChange={handleAnswerSelection}>
-            <input type="radio" name="answer"/>
+          <label className="radio">
+            <input
+              disabled={!!selectedAnswer}
+              onChange={handleAnswerSelection}
+              type="radio"
+              name={`answer-${id}`}
+              value={shuffledAnswers[2]}
+            />
             {shuffledAnswers[2]}
           </label>
-          <label className="radio" onChange={handleAnswerSelection}>
-            <input type="radio" name="answer"/>
+          <label className="radio">
+            <input
+              disabled={!!selectedAnswer}
+              onChange={handleAnswerSelection}
+              type="radio"
+              name={`answer-${id}`}
+              value={shuffledAnswers[3]}
+            />
             {shuffledAnswers[3]}
           </label>
         </div>
       </div>
+      {reveal &&
+        (selectedAnswer === correct
+          ? "Correct!"
+          : `Incorrect! Correct answer is ${correct}`)}
     </section>
   );
 }
